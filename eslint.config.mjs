ecbfb61d5080a6d98eab091afa994cfb7e82bae7
Jordan Jones <jmjones2463@gmail.com>
2024-07-29 10:36:34 -0700

    build: update design tokens, eslint config and all other dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,16 +6,16 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 export default [...compat.extends("@aurodesignsystem/eslint-config"), {
-    rules: {
-        "no-magic-numbers": ["error", {
-            ignore: [0, 1],
-            ignoreArrayIndexes: true,
-        }],
-    },
+  rules: {
+    "no-magic-numbers": ["error", {
+      ignore: [0, 1],
+      ignoreArrayIndexes: true,
+    }],
+  },
 }];
