4b1f97497d64503d97ac4d4680988f94fca297ef
Jordan Jones <jmjones2463@gmail.com>
2024-10-02 14:01:22 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 import { AuroFlight } from './src/auro-flight.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
-export function registerComponent(name) {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroFlight {});
-  }
-}
-
-// Example custom registration
-// registerComponent('my-flight');
+RuntimeUtils.default.prototype.registerComponent('custom-flight', AuroFlight);
