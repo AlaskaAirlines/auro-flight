2ada6338a023e8cf2e0c83690f2b5c0ae7e60ad2
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-30 13:54:26 -0700

    perf: edge case handling in templated code generator

--- scripts/auro-component-template.wca.cjs
+++ scripts/auro-component-template.wca.cjs
@@ -1,12 +1,16 @@
 'use strict'
 module.exports = {
     getTemplatedComponentCode(code) {
-    if (code.includes('customElements.define')) {
+    if (!code || code.includes('customElements.define')) {
       return code;
     }
+
     const defaultTag = code.match(/static register\(name \= (.+)\)/)[1];
     const className = code.match(/export class (.+) extends/)[1];
 
+    if (!defaultTag || !className) {
+      return code;
+    }
     return `
   ${code}
 
