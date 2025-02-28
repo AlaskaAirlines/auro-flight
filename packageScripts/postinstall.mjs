775aebc932cf24f31bf4b8bcfc088bd7714c2d26
Doug <doug@dougflynn.dev>
2025-02-28 15:35:37 -0800

    feat!: move peer dependencies to "real" dependencies
    BREAKING CHANGE: last change was breaking _on a patch version_ because of peer dependencies
    
    Summary:
      Our current build process relies on peer dependencies being present, but
      the peer dependency pipeline is causing far more issues than it's worth.
      Why not just make them regular dependencies? This is what this PR does :)

--- packageScripts/postinstall.mjs
+++ packageScripts/postinstall.mjs
@@ -1,11 +1,10 @@
-'use strict';
-
-import chalk from 'chalk';
-import { createRequire } from 'node:module';
+import chalk from "chalk";
+import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-const pjson = require('../package.json');
+const pjson = require("../package.json");
 
-console.log(chalk.hex('#f26135')(`
+console.log(
+  chalk.hex("#f26135")(`
 
  _______                   __           __ __
 |     __|.---.-.--.--.    |  |--.-----.|  |  |.-----.
@@ -21,8 +20,10 @@ console.log(chalk.hex('#f26135')(`
 ╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
 
         Thanks for installing the latest version
-        of `) + chalk.hex('#ffd200').bold(`${pjson.name} v${pjson.version}.`) + chalk.hex('#f26135')(`
+        of `) +
+    chalk.hex("#ffd200").bold(`${pjson.name} v${pjson.version}.`) +
+    chalk.hex("#f26135")(`
 
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
-`)
+`),
 );
